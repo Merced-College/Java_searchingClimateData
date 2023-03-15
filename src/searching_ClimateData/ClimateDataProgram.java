1eb3e526ccc9af5b9d9acd3337f15dc47bf73bb5
Kathy Kanemoto <kathykanemoto@gmail.com>
2023-03-15 09:51:51 -0700

    added class files

---
+++ src/searching_ClimateData/ClimateDataProgram.java
@@ -0,0 +1,10 @@
+package searching_ClimateData;
+
+public class ClimateDataProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/searching_ClimateData/ClimateDataRecord.java
@@ -0,0 +1,7 @@
+package searching_ClimateData;
+
+public class ClimateDataRecord {
+	
+	//date,AverageTemperature,AverageTemperatureUncertainty,State,Country
+
+}
